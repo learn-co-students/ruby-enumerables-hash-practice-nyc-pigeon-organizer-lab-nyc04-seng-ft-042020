422d937a64b5813fcbbcea3acde5c910c24ebd59
Kyle B <kjburda@gmail.com>
2020-03-23 21:06:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,3 @@
-pigeon_data = {
-        :color => {
-          :purple => ["Theo", "Peter Jr.", "Lucky"],
-          :grey => ["Theo", "Peter Jr.", "Ms. K"],
-          :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-          :brown => ["Queenie", "Alex"]
-        },
-        :gender => {
-          :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-          :female => ["Queenie", "Ms. K"]
-        },
-        :lives => {
-          "Subway" => ["Theo", "Queenie"],
-          "Central Park" => ["Alex", "Ms. K", "Lucky"],
-          "Library" => ["Peter Jr."],
-          "City Hall" => ["Andrew"]
-        }
-      }
-
 def nyc_pigeon_organizer(data)
   new_hash = Hash.new
   data.each do |color_gender_lives, values|
@@ -45,8 +26,5 @@ def nyc_pigeon_organizer(data)
       end #data-dos
     end #values-dos
   end #new_hash
-  p new_hash
-end 
-
-
-nyc_pigeon_organizer(pigeon_data)
+  new_hash
+end
